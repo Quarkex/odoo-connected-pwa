30732995a6272f8cd80fa0884d49367e2016c700
Manlio García <quarkex@gmail.com>
2018-09-20 16:07:43 +0100

    added full fledged CRUD features to the server

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,19 @@ $(document).foundation()
 function refresh_data(string){
     console.log(string);
     var headers = new Headers();
-    fetch('/api', { 'method': 'POST', 'headers': headers, 'body': JSON.stringify({ 'search_string': string }) })
+    fetch('/api/read', { 'method': 'POST', 'headers': headers, 'body': JSON.stringify({ 'search_string': string }) })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            build_list(myJson);
+        });
+}
+
+function delete_entry(id){
+    console.log(id);
+    var headers = new Headers();
+    fetch('/api/delete', { 'method': 'POST', 'headers': headers, 'body': JSON.stringify({ 'target_id': id }) })
         .then(function(response) {
             return response.json();
         })
