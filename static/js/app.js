25c341a13249a556a4313a0b66d3b27d17d237d7
Manlio García <quarkex@gmail.com>
2018-11-13 11:30:32 +0000

    [FIX] Make JS use session system

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,34 @@
 $(document).foundation()
 
-function refresh_data(string){
+var session_token = null;
+
+function login(username, password){
     var headers = new Headers();
-    fetch('/api/read', { 'method': 'POST', 'headers': headers, 'body': JSON.stringify({ 'search_string': string }) })
+    fetch('/api/login', { 'method': 'POST', 'headers': headers, 'body': JSON.stringify({ 'username': username, 'password': password }) })
         .then(function(response) {
             return response.json();
         })
         .then(function(myJson) {
-            build_list(myJson);
+            session_token = myJson['session_token'];
+        });
+}
+login('dev','dev');
+
+function refresh_data(string){
+    var headers = new Headers();
+    fetch('/api/read', { 'method': 'POST', 'headers': headers, 'body': JSON.stringify({ 'session_token': session_token, 'search_string': string }) })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            build_list(myJson['result']);
         });
 }
 
 function delete_entry(id){
     console.log(id);
     var headers = new Headers();
-    fetch('/api/delete', { 'method': 'POST', 'headers': headers, 'body': JSON.stringify({ 'target_id': id }) })
+    fetch('/api/delete', { 'method': 'POST', 'headers': headers, 'body': JSON.stringify({ 'session_token': session_token, 'target_id': id }) })
         .then(function(response) {
             return response.json();
         })
